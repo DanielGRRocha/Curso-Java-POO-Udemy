bc21649afdc98599fe5d52eddc8e37a2cad7ffc6
Daniel Rocha <danrochamaia@gmail.com>
2021-10-11 11:45:44 -0300

    Upload na seção 18 - exemplo (CarRentalProgram) usando interface

---
+++ Seção 18 - Interfaces/src/CarRental/model/entities/Vehicle.java
@@ -0,0 +1,28 @@
+package CarRental.model.entities;
+
+public class Vehicle {
+	
+	private String model;
+	
+	public Vehicle() {
+		
+	}
+
+	public Vehicle(String model) {
+
+		this.model = model;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	
+	
+	
+
+}
---
+++ Seção 18 - Interfaces/src/CarRental/model/services/TaxService.java
@@ -0,0 +1,6 @@
+package CarRental.model.services;
+
+public interface TaxService {
+	
+	double tax(double amount);
+}
